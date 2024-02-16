10b0097f2fa4a4287c7f85800a843a8865ec129f
Pedro <pedrojalvoperezlozao@gmail.com>
2024-02-16 09:42:30 +0100

    acabado

--- src/mates/Matematicas.java
+++ src/mates/Matematicas.java
@@ -1,11 +1,20 @@
 package mates;
-public class Matematicas{
-    /**
-     * Genera una aproximación al número pi mediante el método de
-     * Montecarlo. El parámetro ` pasos` indica el número de puntos
-     * generado.
-     */
-    public static double generarNumeroPiIterativo(long pasos){
-        return 0; // Este código hay que cambiarlo.
+import java.util.Random;
+
+public class Matematicas {
+    public static double generarNumeroPiIterativo(long pasos) {
+        int aciertos = 0;
+        double areaCuadrado = 4.0;
+        Random random = new Random();
+
+        for (int i = 0; i < pasos; i++) {
+            double x = random.nextDouble() * 2.0 - 1.0;
+            double y = random.nextDouble() * 2.0 - 1.0;
+            if (Math.sqrt(x * x + y * y) <= 1.0) {
+                aciertos++;
+            }
+        }
+
+        return areaCuadrado * ((double) aciertos / (double) pasos);
     }
 }
